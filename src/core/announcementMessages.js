adef98dfe4e599757493c910e6415de92ec7a13f
chesdes <mathewches2008@gmail.com>
2026-08-19 18:46:58 +0300

    [feat] new announcement

--- src/core/announcementMessages.js
+++ src/core/announcementMessages.js
@@ -1,7 +1,7 @@
 module.exports = function createAnnouncementMessages({ Discord, Telegram }) {
     return [
-        `Заходи на наш discord-сервер: ${Discord}\nПодписывайся на мой telegram: ${Telegram}`,
-
-        `На нашей комнате есть привилегия VIP!\nВозможности, цена и другие подробности на нашем discord-сервере: ${Discord}`
+        `Заходи на наш Discord-сервер: ${Discord}\nПодписывайся на мой Telegram: ${Telegram}`,
+        `На нашей комнате есть привилегия VIP!\nВозможности, цена и другие подробности на нашем Discord-сервере: ${Discord}`,
+        `Заходи на наш Discord-сервер и привяжи свой Discord-профиль к аккаунту HaxBall!\nПросто напиши команду /link в любом чате сервера.\n${Discord}`
     ];
 };
