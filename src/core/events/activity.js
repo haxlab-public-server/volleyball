eadbf458b676f63590cc87630fbfd97da2952565
chesdes <mathewches2008@gmail.com>
2026-08-29 02:33:37 +0300

    [fix] div float speed 2.2 -> 1.9

--- src/core/events/activity.js
+++ src/core/events/activity.js
@@ -254,7 +254,7 @@ module.exports = function createActivityEvents({
             );
 
             const disc = room.getDiscProperties(0);
-            const boostDivisor = state.serveType === Serve.FLOAT ? 2.2 : 1.5;
+            const boostDivisor = state.serveType === Serve.FLOAT ? 1.9 : 1.5;
             room.setDiscProperties(0, {
                 cGroup: disc.cGroup ^ cf.kick,
                 xspeed: disc.xspeed + Math.round(disc.xspeed / boostDivisor),
